e4399509d973e0d0bfbbce37a7aad72b0ae62221
Ansh Payal <anshpayal754@gmail.com>
2024-07-05 09:51:20 +0530

    Added responsiveness in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom"
 import Logo from "../snipurl-logo.png";
 const Footer = () => {
   return (
-    <footer className="text-white flex justify-between sm:px-16 px-6 items-center border-t border-slate-400">
+    <footer className="text-white flex flex-col sm:flex-row justify-between sm:px-16 px-6 items-center border-t border-slate-400">
       <Link to="/">
       <span className="flex justify-center items-center ">
         <img src={Logo} className=" w-[50px] h-[40px] mr-2"/>
         <p className=" tracking-wide py-10 text-white font-poppins font-semibold text-[16px] sm:text-[20px]">SNIP | URL </p>
       </span>
       </Link>
-      <h3>Copyright Ⓒ 2024 SinpURL. All Rights Reserved.</h3>
+      <h3 className="text-center">Copyright Ⓒ 2024 SinpURL. All Rights Reserved.</h3>
     </footer>
   )
 }
