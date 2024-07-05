308540f3ec791df3f772009e89d99055622634eb
Ansh Payal <anshpayal754@gmail.com>
2024-07-05 09:43:45 +0530

    Added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
-
+import { Link } from "react-router-dom"
+import Logo from "../snipurl-logo.png";
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="text-white flex justify-between sm:px-16 px-6 items-center border-t border-slate-400">
+      <Link to="/">
+      <span className="flex justify-center items-center ">
+        <img src={Logo} className=" w-[50px] h-[40px] mr-2"/>
+        <p className=" tracking-wide py-10 text-white font-poppins font-semibold text-[16px] sm:text-[20px]">SNIP | URL </p>
+      </span>
+      </Link>
+      <h3>Copyright Ⓒ 2024 SinpURL. All Rights Reserved.</h3>
+    </footer>
   )
 }
 
