be32fd460cab86b96ace73afd72af3cb9e144b6e
Ansh Payal <anshpayal754@gmail.com>
2024-07-16 00:17:47 +0530

    Added Logout feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,17 +11,24 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { LinkIcon, LogOut } from "lucide-react"
 import Logo from "../snipurl-logo.png";
+import { UrlState } from "@/Context"
+import useFetch from "@/Hooks/useFetch"
+import { logout } from "@/db/apiAuth"
+import { BarLoader } from "react-spinners"
 
 const Header = () => {
   const navigate = useNavigate();
-  const user = true;
+  const { user, fetchUser } = UrlState();
+
+  const { loading, fetchData: fnLogout } = useFetch(logout);
+
   return (
     <header className=" h-[75px] flex items-center justify-between sm:px-16 px-6 ">
       <Link to="/">
-      <span className="flex justify-center items-center ">
-        <img src={Logo} className=" w-[50px] h-[40px] mr-2"/>
-        <p className=" tracking-wide py-10 font-poppins font-semibold text-[16px] sm:text-[20px]">SNIP | URL </p>
-      </span>
+        <span className="flex justify-center items-center ">
+          <img src={Logo} className=" w-[50px] h-[40px] mr-2" />
+          <p className=" tracking-wide py-10 font-poppins font-semibold text-[16px] sm:text-[20px]">SNIP | URL </p>
+        </span>
       </Link>
       <div>
         {
@@ -29,29 +36,35 @@ const Header = () => {
             <DropdownMenu>
               <DropdownMenuTrigger className="outline-none mt-2">
                 <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarImage src={user?.user_metadata?.profile_pic} className="object-contain" />
                   <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
               </DropdownMenuTrigger>
               <DropdownMenuContent className="mt-4 sm:mr-0 mr-4">
-                <DropdownMenuLabel className="text-black">Ansh Payal</DropdownMenuLabel>
+                <DropdownMenuLabel className="text-black">{user?.user_metadata?.name}</DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>
-                  <LinkIcon color="black" className="w-4 h-4 mr-2"/>
+                  <LinkIcon color="black" className="w-4 h-4 mr-2" />
                   <span className="text-black">Links</span>
                 </DropdownMenuItem>
                 <DropdownMenuItem className="text-red-400">
                   <LogOut color="#ef4444" className="w-4 h-4 mr-2" />
-                  <span className="text-red-500">Logout</span>
+                  <span className="text-red-500" onClick={() => {
+                    fnLogout().then(() => { 
+                      fetchUser();
+                      navigate("/") 
+                    });
+                  }}>Logout</span>
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
           ) : (
 
-            <Button className="font-poppins px-10 font-semibold" variant="outline" onClick={() => { navigate("/auth") }}>Login</Button>
+            <Button className="font-poppins text-black px-10 font-semibold" variant="outline" onClick={() => { navigate("/auth") }}>Login</Button>
           )
         }
       </div>
+      {loading && <BarLoader width={"100%"} color="#475569"/>}
     </header>
   )
 }
